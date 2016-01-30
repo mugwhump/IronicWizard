74e7bcf985f74b483b5535da4013544809b6fb29
Grayson Bartlet <grayson.bartlet@shaw.ca>
2016-01-30 02:30:51 -0800

    basic bonking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//This class has stuff the player does :DDDD
 public class Player : MonoBehaviour {
+	public Collider bonk_collider; //hitbox for bonking stuff
+
+
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown ("Fire1")) {
+			Instantiate (bonk_collider, transform.position + (transform.forward * 2), transform.rotation);
+		}
 	}
 }
---
+++ Assets/Scripts/apply_force.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class apply_force : MonoBehaviour {
+	public float force;
+
+	void OnTriggerEnter (Collider other) {
+		other.GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Impulse);
+	}
+		
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
