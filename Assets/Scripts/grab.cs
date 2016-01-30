5c9e9db7e3949c9b6353898b36d0e924fd6b9dec
Grayson Bartlet <grayson.bartlet@shaw.ca>
2016-01-30 12:25:11 -0800

    Active object will be hilighted Redid grabbing/pushing to use active object

--- Assets/Scripts/grab.cs
+++ Assets/Scripts/grab.cs
@@ -2,18 +2,37 @@
 using System.Collections;
 
 public class grab : MonoBehaviour {
+	public Shader outlineShader;
+
+	Player player;
 	
 	void OnTriggerEnter (Collider other) {
+//		Rigidbody rb = other.GetComponent<Rigidbody>();
+//		if ( rb ) {
+//			GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Player> ().SetGrabbed (rb);
+//		}
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (!player)
+			return; //do nothing if player not instantiated yet
 		Rigidbody rb = other.GetComponent<Rigidbody>();
-		if ( rb ) {
-			GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Player> ().SetGrabbed (rb);
-			Debug.Log ("I graeb u");
+		if ( rb && !rb.CompareTag("Wand")) {
+			player.SetActive (rb);
 		}
 	}
 
+	void OnTriggerExit (Collider other) {
+		Rigidbody rb = other.GetComponent<Rigidbody>();
+		if ( rb ) {
+			player.SetInactive (rb);
+		}
+	}
+		
+
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Player> ();
 	}
 	
 	// Update is called once per frame
