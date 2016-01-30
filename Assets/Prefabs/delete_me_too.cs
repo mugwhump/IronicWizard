40b6388d2742eefd02b541b0436f151c1899f07d
unknown <andrewo@L-VC-10026.corp.shoes.com>
2016-01-29 23:44:39 -0800

    wheeeeeeeeeee folders

---
+++ Assets/Prefabs/delete_me_too.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class delete_me_too : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/also_de_me.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class also_de_me : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
