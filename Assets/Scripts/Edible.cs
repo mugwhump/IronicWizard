29dd3f3d8f88f61e01b2a41cc8095830bbaed948
Grayson Bartlet <grayson.bartlet@shaw.ca>
2016-01-30 14:46:49 -0800

    bonking cube is edible Edible script teleports back to starting position upon being cauldron'd

--- Assets/Scripts/Edible.cs
+++ Assets/Scripts/Edible.cs
@@ -5,4 +5,15 @@ public class Edible : MonoBehaviour {
     public int redValue = 0;
     public int blueValue = 0;
     public int greenValue = 0;
+
+	Vector3 startingPos;
+
+	public void Start() {
+		startingPos = transform.position;
+	}
+
+	public void UseUp(Vector3 originalScale) {
+		transform.position = startingPos;
+		transform.localScale = originalScale;
+	}
 }
