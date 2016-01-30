51d58870a5de1b36145890bd038f7bf03c3f302b
Grayson Bartlet <grayson.bartlet@shaw.ca>
2016-01-30 04:41:31 -0800

    wand is now own prefab instantiated by parent

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,13 +9,14 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-	}
+		//create wand
+		Instantiate (wand, transform.position + new Vector3(-4.15f, 2.1f, 10.3f), Quaternion.FromToRotation(Vector3.up, transform.forward + new Vector3(0,194.3f,0)));
+			} 
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
-			Instantiate (bonkCollider, transform.position + (transform.forward * 2.5f), transform.rotation);
+			Instantiate (bonkCollider, transform.position + (transform.forward * 3f), transform.rotation);
             Instantiate (mouseClickParticle, (wand.transform.position + wand.transform.up), transform.rotation);
 		}
 	}
