d3d7445d411ba00461de3f0a7df12438d01bd854
Grayson Bartlet <grayson.bartlet@shaw.ca>
2016-01-31 04:30:08 -0800

    Bottles play sounds

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Contains arrays of sfx for similar objects
+public class SoundController : MonoBehaviour {
+	public AudioClip[] glass;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	AudioClip RandomSound(AudioClip[] arr) {
+		int ind = Mathf.FloorToInt(Random.Range(0, arr.Length));
+		return arr[ind];
+	}
+
+	public AudioClip GetSound(soundType type){
+		switch (type) {
+		case soundType.glass:
+			return RandomSound (glass);
+			break;
+		}
+		return null;
+	}
+			
+}
---
+++ Assets/Scripts/play_audio_movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum soundType {glass};
+
+//plays object's sound type upon drastic movement
+public class play_audio_movement : MonoBehaviour {
+	public soundType type;
+
+	AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = gameObject.AddComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision other) {
+		Debug.Log ("Play sound!");
+		AudioClip clip = GameObject.FindGameObjectWithTag ("SoundController").GetComponent<SoundController> ().GetSound (type);
+		source.clip = clip;
+		source.Play ();
+	}
+}
