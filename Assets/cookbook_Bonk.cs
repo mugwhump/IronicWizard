288dc899873033f043bae35057f882592e03db3f
Andrew Oatway <aoatway@gmail.com>
2016-01-30 20:12:30 -0800

    scripts for alternative bonking!
    bonk.bonk(Bonk);

---
+++ Assets/cookbook_Bonk.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cookbook_Bonk : Bonkable {
+
+    public bool isOpen = false;
+    private int page = 0;
+
+    void altBonk()
+    {
+        if (isOpen)
+        {
+            //increment through the spritesheet
+            page += 1;
+        }
+        else
+        {
+            //open the cookbook to page 0
+            isOpen = true;
+        }
+    }
+}
